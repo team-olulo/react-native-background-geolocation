7ee87c812d4fec705cd1bdaece3172aaa22274b3
leesam <toyboy2@hanmail.net>
2022-11-22 13:20:07 +0900

    Fixes invalid null provider

--- android/common/src/main/java/com/marianhello/bgloc/provider/RawLocationProvider.java
+++ android/common/src/main/java/com/marianhello/bgloc/provider/RawLocationProvider.java
@@ -46,7 +46,12 @@ public class RawLocationProvider extends AbstractLocationProvider implements Loc
         criteria.setPowerRequirement(Criteria.POWER_HIGH);
 
         try {
-            locationManager.requestLocationUpdates(locationManager.getBestProvider(criteria, true), mConfig.getInterval(), mConfig.getDistanceFilter(), this);
+            String provider = locationManager.getBestProvider(criteria, true);
+            if (provider == null) {
+                logger.warn("No location providers available");
+                return;
+            }
+            locationManager.requestLocationUpdates(provider, mConfig.getInterval(), mConfig.getDistanceFilter(), this);
             isStarted = true;
         } catch (SecurityException e) {
             logger.error("Security exception: {}", e.getMessage());
